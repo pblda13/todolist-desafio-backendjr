60af3ac16ce8459f21a087c2cb43ec90e434fc69
Pâmela Borges <pamela_borges18@hotmail.com>
2024-03-05 14:48:35 -0300

    Criação da entidade todo

---
+++ desafio-liferay-backend/src/main/java/com/desafioliferaybackend/DesafioLiferayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.desafioliferaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiferayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiferayBackendApplication.class, args);
+	}
+
+}
---
+++ desafio-liferay-backend/src/test/java/com/desafioliferaybackend/DesafioLiferayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioliferaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLiferayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
